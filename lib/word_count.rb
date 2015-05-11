824d9cf37c31ee8a69a1cc5a7304f24e746e6b2f
Your Name <you@example.com>
2015-05-11 10:37:58 +0800

    assignment

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,6 +1,12 @@
 class String
-  def unique_word_count
+  def unique_word_count("hello world")
+    unique_word_count == sentence.split(' ').count
+    if unique_word_count == nil
+      puts "0 words"
+    else
+      puts unique_word_count
   end
+end 
   
   def word_frequency_count
   end  
