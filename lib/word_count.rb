d96f982a891b8b0c995e9f7c7b86229d563d3bd6
Dimple Yuen <dimpleyuen@gmail.com>
2015-05-11 10:30:57 +0800

    Dimples W8D1 Assessment

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,26 @@
 class String
   def unique_word_count
+    #remove punctuation
+    self.gsub!(/[^A-Za-z0-9\s]/i, '')
+
+    #splits every word into an array
+    word_array = self.split(/\s+/)
+
+    @@word_count = Hash.new(0)
+
+    #for every word (in the array), make them into keys and +1
+    word_array.each do 
+      | word | @@word_count[word] +=1
+    end
+
+    #return the number of keys
+    return @@word_count.keys.length
+
   end
   
   def word_frequency_count
+    
+    #returns the hash
+    return @@word_count
   end  
 end
