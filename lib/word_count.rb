4d851d128569a290a1b385f57a44712efb1d134a
Xiaopow <xiaoyang88@gmail.com>
2015-05-11 10:34:09 +0800

    xiao's word counter

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,15 @@
 class String
+  def extract_words(original_string)
+    original_string.downcase.scan(/[\w']+/)
+  end
+
   def unique_word_count
+    extract_words(self).length
   end
   
   def word_frequency_count
+    counts = Hash.new(0)
+    extract_words(self).each{ |w| counts[w] += 1 }
+    return counts
   end  
 end
