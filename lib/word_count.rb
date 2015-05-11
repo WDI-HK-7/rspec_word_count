99c0100ce4ca18d68c84f30e2fdbd92f055ea560
Xavi Lozano Segarra <xavils@Xavis-MacBook-Air.local>
2015-05-11 10:31:02 +0800

    not working

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,19 @@
 class String
   def unique_word_count
+  	words_array(self).uniq.count
   end
   
   def word_frequency_count
+  	counts = Hash.new(0)
+
+  	words_array(self).each do |word|
+  		counts[word] += 1
+  	end
+
+  	counts
   end  
-end
+
+  def words_array(string)
+  	string.downcase.scan(/[\W']+/)
+  end
+end 
