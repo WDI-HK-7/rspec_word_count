1d6b313e9dd550e5aa75d5b6fb4046ce938edc6e
youngyang <yyangag@gmail.com>
2015-05-10 20:03:57 -0700

    answers to word_count.rb

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,39 +1,30 @@
-class String
+class String 
   def unique_word_count
-    array_of_individual_words = words_from_string(self)
-    count_unique_words_in_array(array_of_individual_words)
+    count_unique_words_in_array(string) #How do you get the string in the first place? 
   end
-  
+
   def word_frequency_count
-    array_of_individual_words = words_from_string(self)
-    count_of_each_word(array_of_individual_words)
+    count_of_each_word(string) #How do you get the string in the first place? 
   end
-  
+
   private
-  
-  def words_from_string(original_string)
-    # Simpler (non-regex) method is to use .split() instead of .scan()
-    # However, .split() will not remove extranious punctuation. 
-    
-    # This returns an array of individual words, stripped of punctiation and all lowercase
-    original_string.downcase.scan(/[\w']+/)
-  end 
-  
-  def count_unique_words_in_array(original_array_of_words)
-    #Returns the number of unique words
-    original_array_of_words.uniq.count
+
+  def words_from_string(original_string) #remove all spaces, punctuations and store as individual words
+    array = original_string.downcase.split(/\W+/) #returns an array 
   end
-  
+  words_from_string("Crazy is the the best")
+
+  def count_unique_words_in_array(original_array_of_words)
+    original_array_of_words.uniq.count
+  end 
+  count_unique_words_in_array(["crazy", "is", "the", "the", "best"]) # how to call method within a method? 
+
   def count_of_each_word(original_array_of_words)
-    # Creates a new hash, with each default value being 0 for keys that don't exist
-    # Default value of non-existing keys otherwise is nil
-    counts = Hash.new(0)
-    
-    # Iterates through the array of words, uses each word as a key, and increments that key
+    counts = Hash.new(0) 
     original_array_of_words.each do |word|
       counts[word] += 1
     end
-    
-    counts
+    puts counts
   end
-end
+  count_of_each_word(["crazy", "is", "the", " the", "best"])
+end 
