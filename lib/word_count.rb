72b9b12aedcee547d64be0fe13a050ffae6dc4c6
Victoria Li <wingman.li@hotmail.com>
2015-05-11 10:31:40 +0800

    Victoria's quiz

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,15 @@
 class String
-  def unique_word_count
+ 
+  def unique_word_count(user_phrase)
+  	user_phrase.split.uniq.size
   end
   
-  def word_frequency_count
+  def word_frequency_count(user_phrase)
+  	array_of_words = user_phrase.split(' ')
+  	frequencies = Hash.new(0)
+  	array_of_words.each {|word| frequencies[word] +=1 }
+  	return frequencies
   end  
 end
+
+# i dont understand why this gives me wrong number of arguments..... 
